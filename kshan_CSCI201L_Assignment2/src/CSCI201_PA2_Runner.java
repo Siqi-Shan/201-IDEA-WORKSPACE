011e2a57badfec9851eb63adeced7c4c4567fab8
Siqi Shan <kshan@usc.edu>
2020-09-26 00:02:46 -0700

    201 PA2 Commit#11

---
+++ kshan_CSCI201L_Assignment2/src/CSCI201_PA2_Runner.java
@@ -0,0 +1,100 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class CSCI201_PA2_Runner {
+
+    public static InputCodes[] inputParse() {
+        System.out.println("Enter an input file");
+
+        boolean running = true;
+        while (running) {
+            Scanner inputSC = new Scanner(System.in);
+            String path = inputSC.nextLine();
+
+            try {
+                Gson gson = new Gson();
+                BufferedReader file = new BufferedReader(new FileReader(path));
+
+                //ArrayList<InputCodes> codes = gson.fromJson(file, new TypeToken<ArrayList<InputCodes>>(){}.getType());
+                InputCodes[] codes = gson.fromJson(file, InputCodes[].class);
+                //ArrayList<Data> data = gson.fromJson(file, new TypeToken<ArrayList<Data>>(){}.getType());
+                running = false;
+                return codes;
+            }
+            catch (FileNotFoundException error) {
+                System.out.println("File not found, enter a new input file");
+            }
+        }
+
+        return null;
+    }
+
+    public static void handler(Dictionary D) {
+        boolean running = true;
+        while (running) {
+            System.out.println("What would you like to do with your database of codes?\n" +
+                    "\t1) Get frequency of a code\n" +
+                    "\t2) Check if a code was guessed\n" +
+                    "\t3) Remove a code\n" +
+                    "\t4) quit");
+
+            Scanner inputSC = new Scanner(System.in);
+            int option = inputSC.nextInt();
+            String useless = inputSC.nextLine();
+
+            if (option == 1) {
+                System.out.println("Enter a code to check its frequency");
+
+                String toCheck = inputSC.nextLine();
+
+                int frequency = D.frequency(toCheck);
+                System.out.println(toCheck + " was guessed by " + frequency + " teammates");
+            }
+            else if (option == 2) {
+                System.out.println("Enter a code to check if it was guessed by a teammate");
+                String toCheck = inputSC.nextLine();
+
+                boolean result = D.contains(toCheck);
+                if (result) {
+                    System.out.println(toCheck + " was guessed by a teammate");
+                }
+                else {
+                    System.out.println(toCheck + " was not guessed by a teammate");
+                }
+            }
+            else if (option == 3) {
+                System.out.println("Enter a code to remove");
+                String toRemove = inputSC.nextLine();
+
+                int frequency = D.frequency(toRemove);
+                for (int i = 0; i <= frequency; i++) {
+                    D.remove(toRemove);
+                }
+                System.out.println(toRemove + " was removed from your database");
+            }
+            else if (option == 4) {
+                System.out.println("Quitting, have a nice day!");
+                running = false;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        InputCodes[] codes = inputParse();
+        Dictionary<String> InputCodes = new Dictionary<>();
+
+        for (InputCodes temp : codes) {
+            for (int i = 0; i < temp.codes.length; i++) {
+                InputCodes.add((String) temp.codes[i]);
+            }
+        }
+
+        handler(InputCodes);
+    }
+
+}
---
+++ kshan_CSCI201L_Assignment2/src/InputCodes.java
@@ -0,0 +1,10 @@
+public class InputCodes {
+
+    public String[] codes;
+
+//    public InputCodes(Comparable[] input) {
+//        this.codes = input;
+//    }
+
+
+}
